a8b3866cebb33c7043fdaa7e1e0fc575fb3a940a
kesavamurthy-kandeeban <kesavan.k1602@outlook.com>
2021-01-28 00:18:31 +0530

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ fn main() {
     let picture_2_path = args.get(2).unwrap();
     let path_2_ref: &str = &picture_2_path;
 
-    println!("check this 1{:?}", path_1_ref);
-    println!("check this 2{:?}", path_2_ref);
-
     let image1 = raster::open(path_1_ref).unwrap();
     let image2 = raster::open(path_2_ref).unwrap();
 
     let result = compare::equal(&image1, &image2).unwrap();
-    println!("{:?}", result);
+    if result {
+        println!("Pictures are the same")
+    } else {
+        println!("Pictures are different")
+    }
 }
